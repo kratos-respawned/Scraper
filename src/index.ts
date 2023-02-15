8bdb8fdffea6fedf34e4b80e344cdd717bf2558d
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-02-15 20:31:30 +0530

    screenshot

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-console.log("Hi from index.ts");
-console.log("use the command 'npm run dev' to run the typescript files");
-console.log(
-  "use the command 'npm run build' to compile the typescript files to javascript files"
-);
-console.log("use the command 'npm run prod' to run the javascript files");
-console.log(
-  "make sure to use .ts in the import statements for typescript files"
-);
+import puppeteer from "puppeteer";
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://google.com");
+  await page.screenshot({ path: "test.png" });
+  await browser.close();
+})();
