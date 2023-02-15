e09c4ba251607f00cf8c3b330ad17413318699d9
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-02-15 21:01:56 +0530

    fixed viewport

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import puppeteer from "puppeteer";
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
+    defaultViewport: null,
+    userDataDir: "./tmp",
   });
   const page = await browser.newPage();
-  await page.goto("https://google.com");
-  await page.screenshot({ path: "test.png" });
-  await browser.close();
+  await page.goto(
+    "https://www.amazon.in/s?k=gaming+laptop+under+60000&qid=1676473936&sprefix=gaming+lap%2Caps%2C318&ref=sr_pg_1"
+  );
+  const items = await page.$$(
+    ".sg-col-20-of-24 .s-result-item .s-asin .sg-col-0-of-12 .sg-col-16-of-20 .sg-col .s-widget-spacing-small .sg-col-12-of-16"
+  );
+  console.log(items);
 })();
