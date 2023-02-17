d2c8a51806428f87b706e2e69b8edf4b7f205e13
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-02-17 22:10:44 +0530

    added server message

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,6 @@ app.get("/", (req, res) => {
   });
 });
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
